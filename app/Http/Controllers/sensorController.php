b223a0f776f1ab6609a183f70e98bc2a6db78828
ijmert <ijmertu@gmail.com>
2022-05-02 14:20:35 +0200

    update sensorcontroller

--- app/Http/Controllers/sensorController.php
+++ app/Http/Controllers/sensorController.php
@@ -1,19 +1,38 @@
 <?php
 
 namespace App\Http\Controllers;
-use core\controller;
-/**
- * Description of sensorController
- *
- * @author Maarten Vanhengel
- */
-class sensorController {
-    
-    public function siteConfig(){
-        
-    }
-    public function showEditSensor(){
+
+use Illuminate\Http\Request;
+
+class SensorController extends Controller
+{
+     public function showEditSensor(){
        // $this->siteConfig();
-       return view('layouts/editSensorView');
+            //haal content uit database
+         $sensorData['id']= 124;
+         $sensorData['Topic'] = "topic";
+         $sensorData['Naam'] = "sensor1";
+         $sensorData['Maximum'] = "50";
+         $sensorData['Minimum'] = "0";
+         $sensorData['Eenheid'] = "meter";
+         
+         
+        return view("layouts/editSensorView", ['sensorData'=>$sensorData]);
+    }
+    public function editSensor(){
+         if (isset($_POST['AnnuleerBtn'])){
+             return view('layouts/main');
+         } else {
+            $sensorData['id']= $_POST['submitBtn'];
+            $sensorData['Topic'] = $_POST['sensorTopic'];
+            $sensorData['Naam'] = $_POST['sensorTopic'];
+            $sensorData['Maximum'] = $_POST['sensorTopic'];
+            $sensorData['Minimum'] = $_POST['sensorTopic'];
+            $sensorData['Eenheid'] = $_POST['sensorTopic'];
+            
+            //invoegen in database
+            
+            return view('layouts/main');
+         }
     }
 }
