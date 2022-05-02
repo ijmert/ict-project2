9a6096b9d9e793825023443fd297ad402467f3fc
maartenvanhengel <maartenvanhengel2001@gmail.com>
2022-05-02 14:55:50 +0200

    app

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,14 +2,14 @@
 <html lang = "nl">
     <head>
         <title>Sensor Monitoring Tool</title>
-        
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <nav class="navbar-profiel" style="background-color: red;">
             <h1>Sensor Monitoring Tool</h1>
             <span class="dot"></span>
         </nav>
-        <main class="py-4">
+        <main >
             @yield('content')
         </main>
     </body>
