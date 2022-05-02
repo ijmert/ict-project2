7351e441b4c913361a39190f08b08f3e1c78d6fe
maartenvanhengel <maartenvanhengel2001@gmail.com>
2022-05-02 14:33:57 +0200

    test

--- app/Http/Controllers/sensorController.php
+++ app/Http/Controllers/sensorController.php
@@ -23,12 +23,15 @@ class SensorController extends Controller
          if (isset($_POST['AnnuleerBtn'])){
              return view('layouts/main');
          } else {
-             $sensorData['id']= $_POST['sensorTopic'];
-            $sensorData['Topic'] = "topic";
-            $sensorData['Naam'] = "sensor1";
-            $sensorData['Maximum'] = "50";
-            $sensorData['Minimum'] = "0";
-            $sensorData['Eenheid'] = "meter";
+            $sensorData['id']= $_POST['submitBtn'];
+            $sensorData['Topic'] = $_POST['sensorTopic'];
+            $sensorData['Naam'] = $_POST['sensorTopic'];
+            $sensorData['Maximum'] = $_POST['sensorTopic'];
+            $sensorData['Minimum'] = $_POST['sensorTopic'];
+            $sensorData['Eenheid'] = $_POST['sensorTopic'];
+            
+            //invoegen in database
+            
             return view('layouts/main');
          }
     }
