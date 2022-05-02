35475d99e6405ba22d795b38c812dbe794ce4aa6
ijmert <ijmertu@gmail.com>
2022-05-02 15:14:21 +0200

    niks

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,10 +3,20 @@
     <head>
         <title>Sensor Monitoring Tool</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     </head>
     <body>
         <nav class="navbar-profiel" style="background-color: red;">
             <h1>Sensor Monitoring Tool</h1>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
             <span class="dot"></span>
         </nav>
         <main >
